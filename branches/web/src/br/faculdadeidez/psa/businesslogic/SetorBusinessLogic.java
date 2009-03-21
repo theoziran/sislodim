e2d59730d61a61d769df3a5c7d39f9ec376a2a64
manel.pb <manel.pb@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-03-21 21:35:39 +0000

    - início das atividades para o desenvolvimento do item 15 - release 02

---
+++ branches/web/src/br/faculdadeidez/psa/businesslogic/SetorBusinessLogic.java
@@ -0,0 +1,51 @@
+package br.faculdadeidez.psa.businesslogic;
+
+import persistence.antlr.collections.List;
+import br.faculdadeidez.psa.db.DAOSetor;
+import br.faculdadeidez.psa.entity.Setor;
+
+public class SetorBusinessLogic {
+	public String delete(int codigo){
+		try {
+			DAOSetor dSetor = new DAOSetor();
+			Setor setor = dSetor.find( codigo );
+			dSetor.remove(setor);
+			return "removido";
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "problemaRemover";
+		}
+	}
+	
+	public String update(String nome){
+		try {
+			DAOSetor dSetor = new DAOSetor();
+			Setor setor = new Setor(nome);
+			dSetor.update(setor);
+			return "atualizado";
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "problemaAtualizar";
+		}
+	}
+	
+	public String create(String nome){
+		try {
+			DAOSetor dSetor = new DAOSetor();
+			Setor setor = new Setor(nome);
+			dSetor.persist(setor);
+			return "inserido";
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "problemaInserir";
+		}
+	}
+	
+
+	public List listar(){
+		DAOSetor dSetor = new DAOSetor();
+		List setores = dSetor.findAll();
+		return setores;
+		
+	}
+}
