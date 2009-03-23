930c22adc7d0269878c1af270a94f7e1f1e24d2b
josafafilho15 <josafafilho15@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-03-23 13:35:55 +0000

    adição do primeiro teste unitário de UsuarioBusinessLogic

---
+++ branches/web/src/testUsuarioBusinessLogic.java
@@ -0,0 +1,25 @@
+import br.faculdadeidez.psa.businesslogic.UsuarioBusinessLogic;
+import br.faculdadeidez.psa.entity.Usuario;
+import junit.framework.TestCase;
+
+
+public class testUsuarioBusinessLogic extends TestCase {
+
+	public void testCreate(){
+		//TODO adaptar os campos de usuário a nova implementação
+		
+		Usuario u = new Usuario();
+		u.setId(1);
+		u.setLogin("testLogin1");
+		u.setNome("testName1");
+		u.setSenha("testPassword1");
+		u.setAtivo(1);
+		
+		UsuarioBusinessLogic ubl = new UsuarioBusinessLogic();
+		assertEquals("inserido", ubl.create(u));
+	}
+	
+	public void testLogon(){
+		
+	}
+}
