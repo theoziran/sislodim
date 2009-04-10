b50c81f88dbb604d68cc69ad76f47d955df7cbc7
ricardojppb <ricardojppb@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-04-10 21:15:27 +0000

    adicionado um novo projeto mobile

---
+++ .processed/src/br/com/idez/http/TransmissaoDados.java
@@ -0,0 +1,58 @@
+package br.com.idez.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.HttpConnection;
+
+public class TransmissaoDados {
+	/**
+	 * Lembrando que esse método é temporário até que seja apresentado um
+	 * framework para trasmissão de dados que abrange transmissão HTTP O
+	 * framework será aprensentado ou desenvolvido na próxima aula de
+	 * Desenvolvimento para dispositivos móveis (09/04/2009) Alterar os tipos
+	 * para o que melhor adequar
+	 */
+
+	/**
+	 * Essa classes foi testa com um script php e funcionou corretamente no
+	 * nosso caso só precisa substituir o endereço da URL
+	 */
+	private static final String servletHost = "http://localhost/sislodim/index.php?";
+	private static TransmissaoDados instance = null;
+
+	private TransmissaoDados() {
+	
+	}
+
+	public static TransmissaoDados getInstance() {
+		if (instance == null) {
+			instance = new TransmissaoDados();
+		}
+		return instance;
+	}
+
+	public void enviarDados(double longitude, double latitude, int viatura)
+			throws IOException {
+		/**
+		 * Nesse momento peço para o JME abrir uma conexão pra mim, é o JME
+		 * intercede com o device para minha comunicação
+		 */
+		HttpConnection conexao = (HttpConnection) Connector.open(servletHost
+				+ "longitude=" + longitude + "&latitude=" + latitude
+				+ "&viatura=" + viatura);
+
+		// Define o tipo de requisição
+		conexao.setRequestMethod(HttpConnection.GET);
+
+		/**
+		 * Retorno a saída da requisição em um inputStream caso queira fazer
+		 * algum tratamento
+		 */
+		//InputStream inputStream = conexao.openInputStream();
+		//System.out.println(inputStream.toString());
+
+	}
+
+}
---
+++ .processed/src/br/com/idez/ui/FormGPS.java
@@ -0,0 +1,100 @@
+package br.com.idez.ui;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.TextField;
+
+import br.com.idez.core.GPS;
+import br.com.idez.core.UIController;
+
+public class FormGPS extends Form implements CommandListener {
+
+	public static FormGPS instance;
+	private Command cmdExit;
+	private Command cmdStart;
+	private TextField tfLatitude;
+	private TextField tfLongitude;
+
+	private FormGPS(String title) {
+		super(title);
+		cmdExit = new Command("exit", Command.EXIT, 1);
+		cmdStart = new Command("start", Command.OK, 1);
+		//tfLatitude = new TextField("Latitude:", null, 15, TextField.ANY);
+		//tfLongitude = new TextField("Longitude:", null, 15, TextField.ANY);
+
+		addCommand(cmdExit);
+		addCommand(cmdStart);
+
+		setCommandListener(this);
+	}
+
+	private FormGPS(String title, Item[] item) {
+		super(title, item);
+
+	}
+
+	public static FormGPS getInstance() {
+		if (instance == null) {
+			instance = new FormGPS("Ativando GPS...");
+		}
+		return instance;
+	}
+
+	public void commandAction(Command cmd, Displayable displayable) {
+		if (cmd.equals(cmdExit)) {
+			if (displayable instanceof FormGPS) {
+				try {
+					UIController.getInstance().sair();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					// e.printStackTrace();
+				}
+			}
+		}
+		if (cmd.equals(cmdStart)) {
+			if (displayable instanceof FormGPS) {
+				GPS gps = new GPS();
+				gps.start();
+				this.removeCommand(cmdStart);
+				//tfLatitude.setString(Double.toString(gps.getLatitude()));
+				//tfLongitude.setString(Double.toString(gps.getLongitude()));
+				//this.append(tfLatitude);
+				//this.append(tfLongitude);
+				this.append("");
+			}
+		}
+
+	}
+
+	// public void startGps() {
+	// try {
+	// provaider = LocationProvider.getInstance(FormGPS.getCriteria());
+	// System.out.println(provaider);
+	// if (provaider != null) {
+	//
+	// location = provaider.getLocation(5);
+	// System.err.println(location);
+	// coordinates = location.getQualifiedCoordinates();
+	// System.out.println(coordinates);
+	// if (coordinates != null) {
+	// System.out.println(coordinates.getAltitude());
+	// System.out.println(coordinates.getLatitude());
+	// this.append("Latitude: " + coordinates.getLatitude());
+	// this.append("Longitude: " + coordinates.getLongitude());
+	// }
+	//
+	// this.append("buscando Gps....");
+	// }
+	// } catch (LocationException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// } catch (InterruptedException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	// }
+
+}
