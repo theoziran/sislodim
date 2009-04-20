5acc46853d6806a650ef18692d34516567ce7df5
josafafilho15 <josafafilho15@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-04-20 21:19:00 +0000

    Implementação da Validação do CPF de acordo com o algoritmo da Receita Federal

--- src/br/faculdadeidez/psa/businesslogic/UsuarioBusinessLogic.java
+++ src/br/faculdadeidez/psa/businesslogic/UsuarioBusinessLogic.java
@@ -15,8 +15,8 @@ public class UsuarioBusinessLogic {
 	public String logon(String login, String senha) {
 		try {
 			ArrayList<Boolean> errosCamposEmBranco = new ArrayList<Boolean>();
-			errosCamposEmBranco.add(Boolean.valueOf(senha==null));
-			errosCamposEmBranco.add(Boolean.valueOf(login==null));
+			errosCamposEmBranco.add(Boolean.valueOf(senha == null));
+			errosCamposEmBranco.add(Boolean.valueOf(login == null));
 			if (errosCamposEmBranco.contains(Boolean.valueOf(true)))
 				return "camposEmBranco";
 			errosCamposEmBranco.add(Boolean.valueOf(senha.isEmpty()));
@@ -32,12 +32,12 @@ public class UsuarioBusinessLogic {
 					if (obj.getAtivo() == 1) {
 						FacesContext context = FacesContext
 								.getCurrentInstance();
-						if (context != null){
-						HttpSession session = (HttpSession) context
-								.getExternalContext().getSession(false);
-						session.setAttribute("logado", true);
-						session.setAttribute("currentUser", obj);
-						System.out.println("Logado com sucesso");
+						if (context != null) {
+							HttpSession session = (HttpSession) context
+									.getExternalContext().getSession(false);
+							session.setAttribute("logado", true);
+							session.setAttribute("currentUser", obj);
+							System.out.println("Logado com sucesso");
 						}
 						return "logado";
 					}
@@ -75,12 +75,12 @@ public class UsuarioBusinessLogic {
 			}
 			// valida os dados inseridos
 			List<MensagemValidacaoVO> erros = validaDados(user);
-			
-			if(!MensagemValidacao.isValido(erros))
+
+			if (!MensagemValidacao.isValido(erros))
 				return MensagemValidacao.getMensagensValidacao(erros);
-			
+
 			ArrayList<Boolean> errosUserInexistente = new ArrayList<Boolean>();
-			
+
 			UsuarioVO userFind = dUsuario.find(user.getId());
 			errosUserInexistente.add(Boolean.valueOf(userFind == null));
 			List<UsuarioVO> usuarios = dUsuario.findByField("login", user
@@ -105,7 +105,7 @@ public class UsuarioBusinessLogic {
 			if (rgExiste)
 				return "rgExistente";
 			if (!errosUserInexistente.contains(Boolean.valueOf(true))) {
-				
+
 				dUsuario.update(user);
 				return "atualizado";
 			} else {
@@ -122,10 +122,10 @@ public class UsuarioBusinessLogic {
 		try {
 			// valida os dados inseridos
 			List<MensagemValidacaoVO> erros = validaDados(user);
-			
-			if(!MensagemValidacao.isValido(erros))
+
+			if (!MensagemValidacao.isValido(erros))
 				return MensagemValidacao.getMensagensValidacao(erros);
-						
+
 			// inserção
 			DAOUsuario dUsuario = new DAOUsuario();
 			if (dUsuario.findByField("login", user.getLogin()).isEmpty()) {
@@ -156,8 +156,8 @@ public class UsuarioBusinessLogic {
 		return usuarios;
 
 	}
-	
-	public List<UsuarioVO> listarAtivos(){
+
+	public List<UsuarioVO> listarAtivos() {
 		DAOUsuario dUsuario = new DAOUsuario();
 		return dUsuario.findAllActived();
 	}
@@ -171,21 +171,77 @@ public class UsuarioBusinessLogic {
 		return retorno;
 	}
 
-	private List<MensagemValidacaoVO> validaDados(UsuarioVO user){
+	private List<MensagemValidacaoVO> validaDados(UsuarioVO user) {
 		ArrayList<MensagemValidacaoVO> erros = new ArrayList<MensagemValidacaoVO>();
-			
-		erros.add(new MensagemValidacaoVO("CPF", "O CPF é obrigatório", Boolean.valueOf(user.getCpf().isEmpty())));
-		erros.add(new MensagemValidacaoVO("Login", "O login é obrigatório", Boolean.valueOf(user.getLogin().isEmpty())));
-		erros.add(new MensagemValidacaoVO("Nome", "O CPF é obrigatório", Boolean.valueOf(user.getNome().isEmpty())));
-		erros.add(new MensagemValidacaoVO("RG", "O CPF é obrigatório", Boolean.valueOf(user.getRg().isEmpty())));
-		erros.add(new MensagemValidacaoVO("Órgão Expeditor", "O CPF é obrigatório", Boolean.valueOf(user.getOrgExpeditor().isEmpty())));
-		erros.add(new MensagemValidacaoVO("Senha", "O CPF é obrigatório", Boolean.valueOf(user.getSenha().isEmpty())));
-		erros.add(new MensagemValidacaoVO("Login", "O login não conter só números", Boolean.valueOf(user.getLogin().matches("^[0-9]*$"))));
-		erros.add(new MensagemValidacaoVO("Nome", "O nome não pode conter só números", Boolean.valueOf(user.getNome().matches("^[0-9]*$"))));
-		erros.add(new MensagemValidacaoVO("Nome", "O nome contém caracteres inválidos", Boolean.valueOf(!user.getNome().matches(
-				"^[a-zA-ZÁÂÃÀÇÉÊÍÓÔÕÚÜáâãàçéêíóôõúü\\s]*$"))));
+
+		erros.add(new MensagemValidacaoVO("CPF", "O CPF é obrigatório", Boolean
+				.valueOf(user.getCpf().isEmpty())));
+		erros.add(new MensagemValidacaoVO("CPF", "O CPF digitado é inválido",
+				validaCpf(user.getCpf())));
+		erros.add(new MensagemValidacaoVO("Login", "O login é obrigatório",
+				Boolean.valueOf(user.getLogin().isEmpty())));
+		erros.add(new MensagemValidacaoVO("Nome", "O nome é obrigatório",
+				Boolean.valueOf(user.getNome().isEmpty())));
+		erros.add(new MensagemValidacaoVO("RG", "O RG é obrigatório", Boolean
+				.valueOf(user.getRg().isEmpty())));
+		erros.add(new MensagemValidacaoVO("Órgão Expeditor",
+				"O Órgão Expedidor é obrigatório", Boolean.valueOf(user
+						.getOrgExpeditor().isEmpty())));
+		erros.add(new MensagemValidacaoVO("Senha", "A senha é obrigatória",
+				Boolean.valueOf(user.getSenha().isEmpty())));
+		erros.add(new MensagemValidacaoVO("Login",
+				"O login não conter só números", Boolean.valueOf(user
+						.getLogin().matches("^[0-9]*$"))));
+		erros.add(new MensagemValidacaoVO("Nome",
+				"O nome não pode conter só números", Boolean.valueOf(user
+						.getNome().matches("^[0-9]*$"))));
+		erros.add(new MensagemValidacaoVO("Nome",
+				"O nome contém caracteres inválidos", Boolean.valueOf(!user
+						.getNome().matches(
+								"^[a-zA-ZÁÂÃÀÇÉÊÍÓÔÕÚÜáâãàçéêíóôõúü\\s]*$"))));
 		return erros;
 	}
-	
-	
+
+	private boolean validaCpf(String cpf) {
+		if (cpf == null)
+			return false;
+
+		String n = cpf.replaceAll("[^0-9]*", "");
+		boolean isCpf = n.length() == 11;
+
+		if (!isCpf)
+			return false;
+		
+		if (n.equals("00000000000") || n.equals("11111111111")
+				|| n.equals("22222222222") || n.equals("33333333333")
+				|| n.equals("44444444444") || n.equals("55555555555")
+				|| n.equals("66666666666") || n.equals("77777777777")
+				|| n.equals("88888888888") || n.equals("99999999999")) {
+			return false;
+		}
+		
+		int i;
+		int j; 
+		int digit; 
+		int coeficient; 
+		int sum; 
+		int[] foundDv = { 0, 0 }; 
+		int dv1 = Integer.parseInt(String.valueOf(n.charAt(n.length() - 2)));
+		int dv2 = Integer.parseInt(String.valueOf(n.charAt(n.length() - 1)));
+
+		for (j = 0; j < 2; j++) {
+			sum = 0;
+			coeficient = 2;
+			for (i = n.length() - 3 + j; i >= 0; i--) {
+				digit = Integer.parseInt(String.valueOf(n.charAt(i)));
+				sum += digit * coeficient;
+				coeficient++;
+			}
+			foundDv[j] = 11 - sum % 11;
+			if (foundDv[j] >= 10)
+				foundDv[j] = 0;
+		}
+		return dv1 == foundDv[0] && dv2 == foundDv[1];
+	}
+
 }
