98eb992c4c75271c9fb7de32451e47364bea66d7
ricardojppb <ricardojppb@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-05-07 07:55:27 +0000

    implementação que retorna o bairro passando a latitude e longitude

--- branches/web/src/br/faculdadeidez/psa/servico/Aplicacao.java
+++ branches/web/src/br/faculdadeidez/psa/servico/Aplicacao.java
@@ -5,22 +5,27 @@ import java.net.MalformedURLException;
 
 public class Aplicacao {
 public static void main(String[] args) throws ComparacaoDistanciaException {
-	ComparacaoDistancia c = new ComparacaoDistancia();
-		c.setOrigem("-7.1761 -34.8624");
-		c.setDestino("geisel, joão pessoa, paraiba");
-		try {
-			System.out.println(c.getDistancia());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.err.println(e.getMessage());
-		}
-		c.setOrigem("-7.119769,-34.823678");
-		c.setDestino("-7.097614,-34.834213");
-		
-		System.out.println(c.getDistancia());
-		c.setOrigem("-7.183055,-34.839642");
-		c.setDestino("rua maria noemi de souza holanda, mangabeira");
-		
-		System.out.println(c.getDistancia());
+
+  RetornaEndereco re = new RetornaEndereco("-7.1761","-34.8624");
+  System.out.println(re.getBairro(re.PercorrerXml(re.receberXml())));
+	
+	
+//	ComparacaoDistancia c = new ComparacaoDistancia();
+//		c.setOrigem("-7.1761 -34.8624");
+//		c.setDestino("geisel, joão pessoa, paraiba");
+//		try {
+//			System.out.println(c.getDistancia());
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			System.err.println(e.getMessage());
+//		}
+//		c.setOrigem("-7.119769,-34.823678");
+//		c.setDestino("-7.097614,-34.834213");
+//		
+//		System.out.println(c.getDistancia());
+//		c.setOrigem("-7.183055,-34.839642");
+//		c.setDestino("rua maria noemi de souza holanda, mangabeira");
+//		
+//		System.out.println(c.getDistancia());
 	}
 }
