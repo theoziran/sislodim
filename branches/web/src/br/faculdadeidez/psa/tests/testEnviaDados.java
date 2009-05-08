4280d9616b7ec3b0fa6604c0cb213cbd5b9a61d5
glaucoyagami <glaucoyagami@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-05-08 00:21:41 +0000

    Teste Unitario RF06

---
+++ branches/web/src/br/faculdadeidez/psa/tests/testEnviaDados.java
@@ -0,0 +1,47 @@
+package br.faculdadeidez.psa.tests;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.faculdadeidez.psa.apresentacao.servlets.MIDServlet;
+import junit.framework.TestCase;
+import static org.easymock.EasyMock.*;
+
+public class testEnviaDados extends TestCase{
+	/**
+	 * Test case - TC 6.1 - Informação enviada com sucesso
+	 **/
+	public void testaEnviaDados() throws ServletException, IOException{
+		MIDServlet servlet = new MIDServlet();
+		
+		HttpServletRequest requestMock = requestMock();
+		servlet.doGet(requestMock, responseMock());
+		verify(requestMock);
+	}
+
+	private HttpServletResponse responseMock() {
+		HttpServletResponse responseMock = createMock(HttpServletResponse.class);
+		replay(responseMock);
+		return responseMock ;
+	}
+
+	private HttpServletRequest requestMock() {
+		HttpServletRequest requestMock = createMock(HttpServletRequest.class);
+		expect(requestMock.getParameter("codVtr")).andReturn("1234");
+		expect(requestMock.getParameter("latitude")).andReturn("11.123");
+		expect(requestMock.getParameter("longitude")).andReturn("8.923");
+		expect(requestMock.getRequestDispatcher("sucesso.jsp")).andReturn(requestDispatcher());
+		return requestMock;
+	}
+
+	private RequestDispatcher requestDispatcher() {
+		RequestDispatcher dispatcherMock = createNiceMock(RequestDispatcher.class);
+		replay(dispatcherMock);
+		return dispatcherMock;
+	}
+
+}
