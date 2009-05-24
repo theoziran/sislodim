e31ffb09bd1a0933177ae2a8e17340f24c7d36ce
s4mu3l.a.r <s4mu3l.a.r@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-05-24 21:15:46 +0000

    Comentei as classes BairroBean.java e CoordenadaBean.java

--- src/br/faculdadeidez/psa/apresentacao/managedbean/BairroBean.java
+++ src/br/faculdadeidez/psa/apresentacao/managedbean/BairroBean.java
@@ -8,17 +8,35 @@ import javax.faces.model.SelectItem;
 import br.faculdadeidez.psa.vo.BairroVO;
 
 public class BairroBean extends GenericoBean {
+	/**
+	 * Responsável por representar a entidade Bairro
+	 */
 	private BairroVO bairro = new BairroVO();
+	/**
+	 * Responsável por mostrar os objetos na tabela genérica
+	 */
 	private List<BairroVO> listaTudo;
 
+	/**
+	 * FIXME nome do método difere do atributo
+	 * @return
+	 */
 	public BairroVO getSetor() {
 		return bairro;
 	}
 
+	/**
+	 * FIXME nome do método difere do atributo
+	 * @return
+	 */
 	public void setSetor(BairroVO bairro) {
 		this.bairro = bairro;
 	}
 
+	/**
+	 * Método que retorna uma lista de selectItem contendo os bairros cadastrados
+	 * @return List<SelectItem>
+	 */
 	public List<SelectItem> getListBairros() {
 		List<SelectItem> listBairros = new ArrayList<SelectItem>();
 		for (BairroVO bairro : getFachada().listarBairros()) {
@@ -29,7 +47,11 @@ public class BairroBean extends GenericoBean {
 		}
 		return listBairros;
 	}
-
+	
+   /**
+    * Método getter do atributo listaTudo
+    * @return List<BairroVO>
+    */
 	public List<BairroVO> getListaTudo() {
 
 		if (listaTudo == null || listaTudo.isEmpty())
@@ -37,10 +59,11 @@ public class BairroBean extends GenericoBean {
 		return listaTudo;
 	}
 
-	public void setListBairros() {
 
-	}
-
+	/**
+	 * Método setter do atributo listaTudo
+	 * @param bairros Lista de bairros a serem setados
+	 */
 	public void setListaTudo(List<BairroVO> bairros) {
 		this.listaTudo = bairros;
 	}
