7012a53b9b37cbf462b95ea8bccb98591f6c23ef
s4mu3l.a.r <s4mu3l.a.r@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-05-27 14:44:29 +0000

    Comentei os DAO de bairro e cidade

--- src/br/faculdadeidez/psa/db/dao/DAOCidade.java
+++ src/br/faculdadeidez/psa/db/dao/DAOCidade.java
@@ -5,38 +5,76 @@ import java.util.Vector;
 
 import br.faculdadeidez.psa.db.entity.Cidade;
 import br.faculdadeidez.psa.vo.CidadeVO;
-
+/**
+ * Classe que contém todos os métodos específicos de Cidade
+ *
+ */
 public class DAOCidade extends DAOFactory<Cidade> {
+	/**
+	 * Construtor default
+	 */
 	public DAOCidade() {
 		super();
 	}
-	
+	/**
+	 * Encapsulamento do método find genérico
+	 * Recebe a chave do objeto e procura a chave no banco de dados retornando uma entidade
+	 * @param chave int
+	 * @return CidadeVO
+	 */
 	public CidadeVO find(int chave){		
 		return Cidade.VO(super.find(Cidade.class, chave));
 	}
 	
+	/**
+	 * Encapsulamento do método findByField genérico
+	 * Recebe o nome do campo e o valor a ser pesquisado nele
+	 * @param campo String
+	 * @param valor String
+	 * @return List<CidadeVO>
+	 */
 	public List<CidadeVO> findByField(String campo, String valor){
 		return ConvertList(super.findByField(Cidade.class, campo, valor));
 	}
 	
+	/**
+	 * Encapsulamento do método findAll genérico
+	 * Retorna todas as entidades como um select * from
+	 * @return List<CidadeVO>
+	 */
 	public List<CidadeVO> findAll(){
 		return ConvertList(super.findAll(Cidade.class));
 	}
-	
+	/**
+	 * Encapsulamento do método update genérico
+	 * Atualiza a entidade no banco de dados
+	 * @param vo CidadeVO
+	 */
 	public void update(CidadeVO vo){		
 		super.update(new Cidade(vo));
 	}
-	
+	/**
+	 * Encapsulamento do método persist genérico
+	 * Insere a entidade no banco de dados
+	 * @param vo CidadeVO
+	 */
 	public void persist(CidadeVO vo){		
 		super.persist(new Cidade(vo));
 	}
 	
+	/**
+	 * Encapsulamento do método remove genérico
+	 * Remove a linha do banco de dados
+	 * @param vo CidadeVO
+	 */
 	public void remove(CidadeVO vo){		
 		super.remove(new Cidade(vo));
 	}
 	
-	/*
-	 * Converte um List<Tipo1> para um List<Tipo2>
+	/**
+	 * Método que converte uma lista de entidades em Value Object
+	 * @param lista List<Cidade>
+	 * @return List<Cidade>
 	 */
 	public List<CidadeVO> ConvertList(List<Cidade> lista)
 	{
@@ -46,6 +84,11 @@ public class DAOCidade extends DAOFactory<Cidade> {
 		return newLista;
 	}
 	
+	/**
+	 * Método que converte uma lista de Value Object em entidades
+	 * @param lista List<CidadeVO>
+	 * @return List<Cidade>
+	 */
 	public List<Cidade> ConverteEntidade(List<CidadeVO> lista)
 	{
 		List<Cidade> newLista = new Vector<Cidade>();
