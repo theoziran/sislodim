ad64bd9ae0353ba4eadae1cf3f3cc30e857b7aa8
s4mu3l.a.r <s4mu3l.a.r@04deb24a-fa32-11dd-9d1a-b59b2e1864b6>
2009-05-27 14:44:29 +0000

    Comentei os DAO de bairro e cidade

--- branches/web/src/br/faculdadeidez/psa/db/dao/DAOBairro.java
+++ branches/web/src/br/faculdadeidez/psa/db/dao/DAOBairro.java
@@ -5,38 +5,75 @@ import java.util.Vector;
 
 import br.faculdadeidez.psa.db.entity.Bairro;
 import br.faculdadeidez.psa.vo.BairroVO;
-
+/**
+ * Classe onde contém as operações específicas com banco de dados para entidade Bairro
+ *
+ */
 public class DAOBairro extends DAOFactory<Bairro> {
+	/**
+	 * Construtor default
+	 */
 	public DAOBairro() {
 		super();
 	}
 	
+	/**
+	 * Encapsulamento do método find genérico
+	 * Recebe a chave do objeto e procura a chave no banco de dados retornando uma entidade
+	 * @param chave int
+	 * @return BairroVO
+	 */
 	public BairroVO find(int chave){		
 		return Bairro.VO(super.find(Bairro.class, chave));
 	}
-	
+	/**
+	 * Encapsulamento do método findByField genérico
+	 * Recebe o nome do campo e o valor a ser pesquisado nele
+	 * @param campo String
+	 * @param valor String
+	 * @return BairroVO
+	 */
 	public List<BairroVO> findByField(String campo, String valor){
 		return ConvertList(super.findByField(Bairro.class, campo, valor));
 	}
 	
+	/**
+	 * Encapsulamento do método findAll genérico
+	 * Retorna todas as entidades como um select * from
+	 * @return List<BairroVO>
+	 */
 	public List<BairroVO> findAll(){
 		return ConvertList(super.findAll(Bairro.class));
 	}
-	
+	/**
+	 * Encapsulamento do método update genérico
+	 * Atualiza a entidade no banco de dados
+	 * @param vo BairroVO
+	 */
 	public void update(BairroVO vo){		
 		super.update(new Bairro(vo));
 	}
-	
+	/**
+	 * Encapsulamento do método persist genérico
+	 * Insere a entidade no banco de dados
+	 * @param vo BairroVO
+	 */
 	public void persist(BairroVO vo){		
 		super.persist(new Bairro(vo));
 	}
-	
+	/**
+	 * Encapsulamento do método remove genérico
+	 * Remove a linha do banco de dados
+	 * @param vo BairroVO
+	 */
 	public void remove(BairroVO vo){		
 		super.remove(new Bairro(vo));
 	}
 	
-	/*
-	 * Converte um List<Tipo1> para um List<Tipo2>
+	/**
+	 * Método que converte uma lista de entidades Bairro em BairroVO
+	 * @param lista List<Bairro>
+	 * @return List<BairroVO>
 	 */
 	public List<BairroVO> ConvertList(List<Bairro> lista)
 	{
@@ -45,7 +82,11 @@ public class DAOBairro extends DAOFactory<Bairro> {
 			newLista.add(Bairro.VO(set));		
 		return newLista;
 	}
-	
+	/**
+	 * Método que converte uma lista de Value Object de bairro em entidades Bairro
+	 * @param lista List<BairroVO>
+	 * @return List<Bairro>
+	 */
 	public List<Bairro> ConverteEntidade(List<BairroVO> lista)
 	{
 		List<Bairro> newLista = new Vector<Bairro>();
